4e9e5c409a83f9172bb5907be04a4af8ed8ae93a
techkamar <techkamar@gmail.com>
2024-07-10 18:46:33 +0530

    Dummy Post CSS ready

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -5,11 +5,25 @@
     export let like_count;
 </script>
 
-<section>
-    <div>{content}</div>
-    <div>
+<section class="box">
+    <div class="content-box">{content}</div>
+    <div class="info-box">
         <span>{comment_count} Comments</span>
         <span>{share_count} Shares</span>
         <span>{like_count} Likes</span>
     </div>
 </section>
+
+<style>
+    .box{
+        border: 1px solid black;
+    }
+    .info-box{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    .content-box{
+        padding-left:20px;
+        background-color: aqua;
+    }
+</style>
