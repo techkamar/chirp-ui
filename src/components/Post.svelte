c30251aca27a6f8da791bdca282a8f567333054c
techkamar <techkamar@gmail.com>
2024-07-11 16:22:26 +0530

    Replies are coming

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -6,24 +6,35 @@
     export let like_count;
 	import {getCallResponseJSON} from '../lib/js/util';
     import {BACKEND_HOST, GET_ALL_POST_COMMENTS} from '../lib/js/constants';
+    import Post from './Post.svelte';
 
     let commentsExpanded = false;
     let comments = [];
 
     async function getAllComments(postID){
         let url = BACKEND_HOST+GET_ALL_POST_COMMENTS(postID);
-		let replies = await getCallResponseJSON(url);
-		console.log(replies);
+		comments = await getCallResponseJSON(url);
+		commentsExpanded = true;
     }
 </script>
 
 <section class="box">
     <div class="content-box">{content}</div>
     <div class="info-box">
-        <div class="info-box-item" on:click={()=> {getAllComments(id)}}>{comment_count} Comments</div>
+        <div class="info-box-item" on:click={()=> {getAllComments(id)}}>
+            {comment_count} Comments
+        </div>
         <span class="info-box-item">{share_count} Shares</span>
         <span class="info-box-item">{like_count} Likes</span>
     </div>
+    {#if commentsExpanded}
+    <div class="reply-box">
+        <p> Replies</p>
+        {#each comments as comment,i}
+        <Post id = {comment.id} content={comment.content} comment_count={comment.commentedby} like_count={comment.likedby} share_count={comment.sharedby+comment.quotedby} />
+        {/each}
+    </div>
+    {/if}
 </section>
 
 <style>
@@ -41,5 +52,9 @@
     .info-box-item:hover{
         background-color: black;
         color: white;
+        cursor: pointer;
+    }
+    .reply-box{
+        margin-left:20px;
     }
 </style>
