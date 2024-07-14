ce821a7f78f27f066328d8fb6a610d63b62a659c
msi <msi@MSI-GL62M-7RDX.MSI>
2024-07-14 14:02:51 +0530

    Added PostID route

---
+++ src/routes/post/[postID]/+page.server.js
@@ -0,0 +1,7 @@
+export function load({ params }) {
+	const parameter = params;
+
+	return {
+		parameter
+	};
+}
---
+++ src/routes/post/[postID]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let data;
+</script>
+
+<h1>post ID = {data.parameter.postID}</h1>
