945024e0b35a1b2cc19290ae52019b33561883ca
techkamar <techkamar@gmail.com>
2024-07-16 14:19:25 +0530

    Added dynamic love button

--- src/components/PostInfo.svelte
+++ src/components/PostInfo.svelte
@@ -5,12 +5,15 @@
     import {onMount} from 'svelte';
     import Post from './Post.svelte';
     import MakeComment from './MakeComment.svelte';
-    import PostInfo from './PostInfo.svelte'
+    import PostInfo from './PostInfo.svelte';
+    import EmptyLove from './PostInfoComp/EmptyLove.svelte';
+    import FilledLove from './PostInfoComp/FilledLove.svelte';
 
     export let postID;
     export let comment_count;
     export let share_count;
     export let like_count;
+    export let isLiked = false;
     export let loadComments = false;
     export let commentsExpanded = false;
     export let comments = [];
@@ -21,18 +24,23 @@
         }
     }
 
+    async function unlikePost(){
+        // Already Liked. Then Unlike
+        let unlikeURL = BACKEND_HOST+UNLIKE_A_POST(postID);
+        await postCallEmptyPayload(unlikeURL);
+        like_count-=1;
+        isLiked = false;
+    }
     async function likePost()
     {
         let url = BACKEND_HOST+LIKE_A_POST(postID);
 		let likeResp = await postCallEmptyPayload(url);
         if(likeResp.status==200){
+            isLiked= true;
             like_count+=1;
         }
         else if(likeResp.status==405){
-            // Already Liked. Then Unlike
-            let unlikeURL = BACKEND_HOST+UNLIKE_A_POST(postID);
-            await postCallEmptyPayload(unlikeURL);
-            like_count-=1;
+            await unlikePost();
         }
     }
 
@@ -57,8 +65,11 @@
         <svg viewBox="0 0 24 24" class="icon-svg" width="20px" height="20px"><g><path d="M4.5 3.88l4.432 4.14-1.364 1.46L5.5 7.55V16c0 1.1.896 2 2 2H13v2H7.5c-2.209 0-4-1.79-4-4V7.55L1.432 9.48.068 8.02 4.5 3.88zM16.5 6H11V4h5.5c2.209 0 4 1.79 4 4v8.45l2.068-1.93 1.364 1.46-4.432 4.14-4.432-4.14 1.364-1.46 2.068 1.93V8c0-1.1-.896-2-2-2z"></path></g></svg>
         {share_count}</span>
     <span class="info-box-item" on:click={()=>{likePost()}}>
-        <svg viewBox="0 0 24 24" class="icon-svg" width="20px" height="20px" fill="black">
-            <g><path d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path></g></svg>
+        {#if isLiked}
+            <FilledLove/>
+        {:else}
+	        <EmptyLove/>
+        {/if}
         {like_count}
     </span>            
 </div>
